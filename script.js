2c5f1d587e087ecc3e7ca3319e730999d51b0f67
Scott Alton <scottaltonmusic@gmail.com>
2020-06-09 09:47:33 -0400

    Data now pushed to local storage and UI updated accordingly on page load.

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 // DOM Elements
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".row.seat:not(.occupied)");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
-// + sign equaivalent to parseInt function
+// + sign equivalent to parseInt function
 let ticketPrice = +movieSelect.value;
+populateUI();
 
 // Functions
+// updateSelectedCount function - updates quantity and price, and keeps track of which seats have been selected for saving in local storage
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  // Preparing selected seats for local storage by adding index to each seat
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  // Set items to local storage
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// populateUI function - retrieve data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+// setMovieData function - update movie data depending on which film is selected so proper info can be set to local storage
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 // Event Listeners
 // Movie Selection Event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  // Update movie data for local storage
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
 });
 
 // Seat Click Event
@@ -32,3 +67,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+// Initial count and total set
+updateSelectedCount();
